7f8aabf3203feaea6ee24cc23d78c8517096ee3d
jcchadwell <josephchadwell2@gmail.com>
2023-02-24 22:36:49 -0600

    Initial Commit

---
+++ src/main/java/com/teamgalactic/aztcg/AztcgApplication.java
@@ -0,0 +1,19 @@
+package com.teamgalactic.aztcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@ComponentScan({"com.teamgalactic.aztcg.controller", "com.teamgalactic.aztcg.service"})
+@EntityScan("com.teamgalactic.entity")
+@EnableJpaRepositories("com.teamgalactic.repository")
+public class AztcgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AztcgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamgalactic/aztcg/AztcgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamgalactic.aztcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AztcgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
