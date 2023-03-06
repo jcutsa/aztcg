89b986127c6b769363093e8d89d6b0be44dcc876
jcchadwell <josephchadwell2@gmail.com>
2023-03-05 22:59:16 -0600

    Added CardType, Card, and User

---
+++ src/main/java/com/teamgalactic/aztcg/entity/Card.java
@@ -0,0 +1,24 @@
+package com.teamgalactic.aztcg.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="card")
+public class Card {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="card_id")
+	private Long id;
+	
+	//TODO: Card Type enum
+}
