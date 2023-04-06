6d174a93fad1a9a38fd548b7048ff3903acb9dc9
Ian Uriegas <ianuriegas@ians-air.lan>
2023-04-06 11:57:39 -0500

    fixed errors in HomePage from previous commit

--- front-end/src/pages/HomePage.js
+++ front-end/src/pages/HomePage.js
@@ -1,26 +1,17 @@
 import React from 'react';
 
 export default function HomePage() {
-<<<<<<< HEAD
   const handleLogout = async () => {
     const response = await fetch('/logout');
     window.location.href = `${response.logoutUrl}&returnTo=${window.location.origin}`;
   };
 
-=======
->>>>>>> 29b1f2b1 (added search/filter for product page)
   return (
     <div>
       <h1>Home Page</h1>
       <p>Welcome to our trading card website</p>
-<<<<<<< HEAD
       <button onClick={handleLogout}>Logout</button>
     </div>
   );
 }
 
-=======
-    </div>
-  );
-}
->>>>>>> 29b1f2b1 (added search/filter for product page)
