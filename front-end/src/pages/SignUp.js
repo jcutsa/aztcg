f585d4d330a27a859e4262c834d7270facfe7b68
Ian Uriegas <ianuriegas@gmail.com>
2023-04-17 14:45:17 -0500

    routed registration page

--- front-end/src/pages/SignUp.js
+++ front-end/src/pages/SignUp.js
@@ -115,9 +115,9 @@ export default function SignUp() {
             >
               Sign Up
             </Button>
-            <Grid container justifyContent="flex-end">
+            <Grid container justifyContent="center">
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/sign-in" variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
