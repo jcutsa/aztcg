0ebe2c4dcb2401b4abb2bd27754b6c7141133e8d
me <91627805+agalindo96@users.noreply.github.com>
2023-04-30 21:45:35 -0500

    formatted price decimal places

--- front-end/src/components/Card.js
+++ front-end/src/components/Card.js
@@ -4,52 +4,60 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function myCard(props) {
-  const { name, image, price, inStock, brand, id } = props;
-  const navigate = useNavigate();
-  const [elevation, setElevation] = useState(3);
+    const { name, image, price, inStock, brand, id } = props;
+    const navigate = useNavigate();
+    const [elevation, setElevation] = useState(3);
 
-  function raiseElevation() {
-    setElevation(20);
-  }
+    function raiseElevation() {
+        setElevation(20);
+    }
 
-  function lowerElevation() {
-    setElevation(5);
-  }
+    function lowerElevation() {
+        setElevation(5);
+    }
 
-  const handleButtonClick = () => {
-    navigate("/card/" + id);
-  };
+    const handleButtonClick = () => {
+        navigate("/card/" + id);
+    };
 
-  return (
-    <Card
-      elevation={elevation}
-      onMouseEnter={raiseElevation}
-      onMouseLeave={lowerElevation}
-      sx={{
-        cursor: "pointer",
-        height: "fit-content",
-        width: "500px",
-        padding: "5px",
-      }}
-    >
-      <Stack direction={"row"}>
-        <img
-          onClick={handleButtonClick}
-          src={image}
-          alt={name}
-          style={{ height: "300px" }}
-        />
-        <Stack spacing={2} onClick={handleButtonClick}>
-          <h2 style={{ marginBottom: "0" }} onClick={handleButtonClick}>
-            {name}
-          </h2>
-          <p>${price.toFixed(2)}</p>
-          <p>{brand}</p>
-          <p style={{ color: inStock ? "green" : "red", fontWeight: "bold" }}>
-            {inStock ? "In Stock" : "Out of Stock"}
-          </p>
-        </Stack>
-      </Stack>
-    </Card>
-  );
+    return (
+        <Card
+            elevation={elevation}
+            onMouseEnter={raiseElevation}
+            onMouseLeave={lowerElevation}
+            sx={{
+                cursor: "pointer",
+                height: "fit-content",
+                width: "500px",
+                padding: "5px",
+            }}
+        >
+            <Stack direction={"row"}>
+                <img
+                    onClick={handleButtonClick}
+                    src={image}
+                    alt={name}
+                    style={{ height: "300px" }}
+                />
+                <Stack spacing={2} onClick={handleButtonClick}>
+                    <h2
+                        style={{ marginBottom: "0" }}
+                        onClick={handleButtonClick}
+                    >
+                        {name}
+                    </h2>
+                    <p>${price.toFixed(2)}</p>
+                    <p>{brand}</p>
+                    <p
+                        style={{
+                            color: inStock ? "green" : "red",
+                            fontWeight: "bold",
+                        }}
+                    >
+                        {inStock ? "In Stock" : "Out of Stock"}
+                    </p>
+                </Stack>
+            </Stack>
+        </Card>
+    );
 }
--- front-end/src/pages/SingleCard.js
+++ front-end/src/pages/SingleCard.js
@@ -1,10 +1,10 @@
 import {
-  Card,
-  Stack,
-  Typography,
-  Button,
-  Select,
-  MenuItem,
+    Card,
+    Stack,
+    Typography,
+    Button,
+    Select,
+    MenuItem,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -14,131 +14,144 @@ import defaultImage from "../assetts/images/card1.jpg";
 // import cards from "../assetts/CardsData";
 
 function SingleCard({ setUser, loggedIn }) {
-  const { cardId } = useParams();
-  const [card, setCard] = useState({});
-  const [selectedQuantity, setSelectedQuantity] = useState(1);
-  const [cartItems, setCartItems] = useState([]);
+    const { cardId } = useParams();
+    const [card, setCard] = useState({});
+    const [selectedQuantity, setSelectedQuantity] = useState(1);
+    const [cartItems, setCartItems] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get(`http://localhost:8080/api/product/${cardId}`)
-      .then((response) => {
-        const cardData = response.data;
-        const formattedCard = {
-          name: cardData.name,
-          brand: cardData.card_type.name,
-          image: cardData.image_url || defaultImage,
-          price: cardData.price,
-          inStock: cardData.quantity_on_hand > 0,
-          quantity: cardData.quantity_on_hand,
-          id: cardData.id.toString(),
+    useEffect(() => {
+        axios
+            .get(`http://localhost:8080/api/product/${cardId}`)
+            .then((response) => {
+                const cardData = response.data;
+                const formattedCard = {
+                    name: cardData.name,
+                    brand: cardData.card_type.name,
+                    image: cardData.image_url || defaultImage,
+                    price: cardData.price.toFixed(2),
+                    inStock: cardData.quantity_on_hand > 0,
+                    quantity: cardData.quantity_on_hand,
+                    id: cardData.id.toString(),
+                };
+                setCard(formattedCard);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, [cardId]);
+
+    const handleAddToCart = () => {
+        const newItem = {
+            name: card.name,
+            brand: card.brand,
+            image: card.image,
+            price: card.price,
+            maxQuantity: card.quantity,
+            quantitySelected: selectedQuantity,
+            id: card.id,
+            totalPrice: card.price * selectedQuantity,
         };
-        setCard(formattedCard);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [cardId]);
-
-  const handleAddToCart = () => {
-    const newItem = {
-      name: card.name,
-      brand: card.brand,
-      image: card.image,
-      price: card.price,
-      maxQuantity: card.quantity,
-      quantitySelected: selectedQuantity,
-      id: card.id,
-      totalPrice: card.price * selectedQuantity,
+        setUser((prevUser) => ({
+            ...prevUser,
+            cart: [...prevUser.cart, newItem],
+        }));
+        console.log(
+            `Added ${selectedQuantity} ${card.name} to cart // Card id: ${card.id}`
+        );
     };
-    setUser((prevUser) => ({
-      ...prevUser,
-      cart: [...prevUser.cart, newItem],
-    }));
-    console.log(
-      `Added ${selectedQuantity} ${card.name} to cart // Card id: ${card.id}`
+
+    const quantityOptions = Array.from(
+        { length: card.quantity },
+        (_, i) => i + 1
     );
-  };
 
-  const quantityOptions = Array.from(
-    { length: card.quantity },
-    (_, i) => i + 1
-  );
+    return (
+        <div style={{ padding: "24px 48px", textAlign: "center" }}>
+            <p>
+                <Stack
+                    direction={"row"}
+                    flex="wrap"
+                    justifyContent={"space-evenly"}
+                >
+                    <img
+                        src={card.image}
+                        alt={card.name}
+                        style={{ width: "30%", height: "auto" }}
+                    />
+                    <Stack direction={"column"}>
+                        <h2>{card.name}</h2>
+                        {!loggedIn ? (
+                            <Typography>
+                                Please sign in to view stock and add to cart
+                            </Typography>
+                        ) : (
+                            <Card sx={{ padding: "24px" }}>
+                                <Stack direction={"column"}>
+                                    <h3>${card.price}</h3>
+                                </Stack>
+                                {card.inStock ? (
+                                    <Stack
+                                        direction={"row"}
+                                        spacing={2}
+                                        alignItems={"center"}
+                                        justifyContent={"center"}
+                                        mt={2}
+                                    >
+                                        <Select
+                                            value={selectedQuantity}
+                                            onChange={(event) =>
+                                                setSelectedQuantity(
+                                                    event.target.value
+                                                )
+                                            }
+                                            sx={{ width: 120 }} // set the width to 120 pixels
+                                        >
+                                            {quantityOptions.map((quantity) => (
+                                                <MenuItem
+                                                    key={quantity}
+                                                    value={quantity}
+                                                >
+                                                    {quantity}
+                                                </MenuItem>
+                                            ))}
+                                        </Select>
+                                        <Button
+                                            sx={{
+                                                color: "black",
+                                                pointerEvents: "none",
+                                                textTransform: "lowercase",
+                                            }}
+                                        >
+                                            of {card.quantity}
+                                        </Button>
 
-  return (
-    <div style={{ padding: "24px 48px", textAlign: "center" }}>
-      <p>
-        <Stack direction={"row"} flex="wrap" justifyContent={"space-evenly"}>
-        <img
-  src={card.image}
-  alt={card.name}
-  style={{ width: "30%", height: "auto" }}
-/>
-<Stack direction={"column"}>
-  <h2>{card.name}</h2>
-  {!loggedIn ? (
-    <Typography>Please sign in to view stock and add to cart</Typography>
-  ) : (
-    <Card sx={{ padding: "24px" }}>
-      <Stack direction={"column"}>
-        <h3>${card.price}</h3>
-      </Stack>
-      {card.inStock ? (
-        <Stack
-          direction={"row"}
-          spacing={2}
-          alignItems={"center"}
-          justifyContent={"center"}
-          mt={2}
-        >
-          <Select
-            value={selectedQuantity}
-            onChange={(event) =>
-              setSelectedQuantity(event.target.value)
-            }
-            sx={{ width: 120 }} // set the width to 120 pixels
-          >
-            {quantityOptions.map((quantity) => (
-              <MenuItem key={quantity} value={quantity}>
-                {quantity}
-              </MenuItem>
-            ))}
-          </Select>
-          <Button
-            sx={{
-              color: "black",
-              pointerEvents: "none",
-              textTransform: "lowercase",
-            }}
-          >
-            of {card.quantity}
-          </Button>
-
-          <Button
-            variant={"contained"}
-            onClick={handleAddToCart}
-            sx={{
-              backgroundColor: "green",
-              color: "white",
-              "&:hover": { backgroundColor: "darkgreen" },
-            }}
-          >
-            Add to Cart
-          </Button>
-        </Stack>
-      ) : (
-        <Button variant={"outlined"} disabled>
-          Out of Stock
-        </Button>
-      )}
-    </Card>
-  )}
-</Stack>
-
-        </Stack>
-      </p>
-    </div>
-  );
+                                        <Button
+                                            variant={"contained"}
+                                            onClick={handleAddToCart}
+                                            sx={{
+                                                backgroundColor: "green",
+                                                color: "white",
+                                                "&:hover": {
+                                                    backgroundColor:
+                                                        "darkgreen",
+                                                },
+                                            }}
+                                        >
+                                            Add to Cart
+                                        </Button>
+                                    </Stack>
+                                ) : (
+                                    <Button variant={"outlined"} disabled>
+                                        Out of Stock
+                                    </Button>
+                                )}
+                            </Card>
+                        )}
+                    </Stack>
+                </Stack>
+            </p>
+        </div>
+    );
 }
 
 export default SingleCard;
