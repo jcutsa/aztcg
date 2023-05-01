b955a3dbe993f1ac77d86be41b5d3c99333de66a
Ian Uriegas <ianuriegas@gmail.com>
2023-05-01 10:19:33 -0500

    fixed single card price formatting to 2 decimal places

--- front-end/src/pages/SingleCard.js
+++ front-end/src/pages/SingleCard.js
@@ -69,72 +69,73 @@ function SingleCard({ setUser, loggedIn }) {
     <div style={{ padding: "24px 48px", textAlign: "center" }}>
       <p>
         <Stack direction={"row"} flex="wrap" justifyContent={"space-evenly"}>
-        <img
-  src={card.image}
-  alt={card.name}
-  style={{ width: "30%", height: "auto" }}
-/>
-<Stack direction={"column"}>
-  <h2>{card.name}</h2>
-  {!loggedIn ? (
-    <Typography>Please sign in to view stock and add to cart</Typography>
-  ) : (
-    <Card sx={{ padding: "24px" }}>
-      <Stack direction={"column"}>
-        <h3>${card.price}</h3>
-      </Stack>
-      {card.inStock ? (
-        <Stack
-          direction={"row"}
-          spacing={2}
-          alignItems={"center"}
-          justifyContent={"center"}
-          mt={2}
-        >
-          <Select
-            value={selectedQuantity}
-            onChange={(event) =>
-              setSelectedQuantity(event.target.value)
-            }
-            sx={{ width: 120 }} // set the width to 120 pixels
-          >
-            {quantityOptions.map((quantity) => (
-              <MenuItem key={quantity} value={quantity}>
-                {quantity}
-              </MenuItem>
-            ))}
-          </Select>
-          <Button
-            sx={{
-              color: "black",
-              pointerEvents: "none",
-              textTransform: "lowercase",
-            }}
-          >
-            of {card.quantity}
-          </Button>
+          <img
+            src={card.image}
+            alt={card.name}
+            style={{ width: "30%", height: "auto" }}
+          />
+          <Stack direction={"column"}>
+            <h2>{card.name}</h2>
+            {!loggedIn ? (
+              <Typography>
+                Please sign in to view stock and add to cart
+              </Typography>
+            ) : (
+              <Card sx={{ padding: "24px" }}>
+                <Stack direction={"column"}>
+                  <h3>${card.price?.toFixed(2)}</h3>
+                </Stack>
+                {card.inStock ? (
+                  <Stack
+                    direction={"row"}
+                    spacing={2}
+                    alignItems={"center"}
+                    justifyContent={"center"}
+                    mt={2}
+                  >
+                    <Select
+                      value={selectedQuantity}
+                      onChange={(event) =>
+                        setSelectedQuantity(event.target.value)
+                      }
+                      sx={{ width: 120 }} // set the width to 120 pixels
+                    >
+                      {quantityOptions.map((quantity) => (
+                        <MenuItem key={quantity} value={quantity}>
+                          {quantity}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                    <Button
+                      sx={{
+                        color: "black",
+                        pointerEvents: "none",
+                        textTransform: "lowercase",
+                      }}
+                    >
+                      of {card.quantity}
+                    </Button>
 
-          <Button
-            variant={"contained"}
-            onClick={handleAddToCart}
-            sx={{
-              backgroundColor: "green",
-              color: "white",
-              "&:hover": { backgroundColor: "darkgreen" },
-            }}
-          >
-            Add to Cart
-          </Button>
-        </Stack>
-      ) : (
-        <Button variant={"outlined"} disabled>
-          Out of Stock
-        </Button>
-      )}
-    </Card>
-  )}
-</Stack>
-
+                    <Button
+                      variant={"contained"}
+                      onClick={handleAddToCart}
+                      sx={{
+                        backgroundColor: "green",
+                        color: "white",
+                        "&:hover": { backgroundColor: "darkgreen" },
+                      }}
+                    >
+                      Add to Cart
+                    </Button>
+                  </Stack>
+                ) : (
+                  <Button variant={"outlined"} disabled>
+                    Out of Stock
+                  </Button>
+                )}
+              </Card>
+            )}
+          </Stack>
         </Stack>
       </p>
     </div>
